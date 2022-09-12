1e4e07f3f80b6eb4acf0974d6eda81d71ccf384a
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-12 09:24:00 +0300

    added distance between points and english name of the last digit

---
+++ src/01. Basic Syntax, Conditional Statements and Loops/englishNameOfTheLastDigit.ts
@@ -0,0 +1,34 @@
+function englishNameOfTheLastDigit(number: number) {
+    let lastDigit = number % 10;
+
+    if (lastDigit < 0) {
+        lastDigit = Math.abs(lastDigit);
+    }
+
+    switch (lastDigit) {
+        case 0:
+            return "zero";
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+    }
+}
+
+console.log(englishNameOfTheLastDigit(512));
+console.log(englishNameOfTheLastDigit(1));
+console.log(englishNameOfTheLastDigit(1643));
