97df3a2ba555740136b6e7e0078f26da33fd7d4f
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-12 11:45:28 +0300

    reverse string and sort numbers

---
+++ src/01. Basic Syntax, Conditional Statements and Loops/reverseString.ts
@@ -0,0 +1,8 @@
+function reverseString(input: string) {
+    const newWord = input.split("").reverse().join("");
+    console.log(newWord);
+}
+
+reverseString('Hello');
+reverseString('SoftUni');
+reverseString('1234');
---
+++ src/01. Basic Syntax, Conditional Statements and Loops/sortNumbers.ts
@@ -0,0 +1,8 @@
+function sortNumbers(num: number[]) {
+    num.sort((a, b) => b - a);
+    console.log(num.join('\n'));
+}
+
+sortNumbers([2, 1, 3]);
+sortNumbers([-2, 1, 3]);
+sortNumbers([0, 0, 2]);
