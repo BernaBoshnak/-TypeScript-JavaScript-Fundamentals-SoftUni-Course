be03a7eb9c91ef077901240eb7a62cbd5c977873
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-17 21:38:49 +0300

    concatenate names solution

---
+++ src/02. Data Types and Variables/concatenateNames.ts
@@ -0,0 +1,11 @@
+function concatenateNames(
+  firstName: string,
+  secondName: string,
+  delimeter: string
+) {
+  console.log(`${firstName}${delimeter}${secondName}`);
+}
+
+concatenateNames("John", "Smith", "->");
+concatenateNames("Jan", "White", "<->");
+concatenateNames("Linda", "Terry", "=>");
