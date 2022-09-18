68b32ebe31603964189bbd6694919da3fe269c41
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-18 16:05:10 +0300

    integer and float solution

---
+++ src/02. Data Types and Variables/integerAndFloat.ts
@@ -0,0 +1,13 @@
+function integerAndFloat(
+  firstNumber: number,
+  seconNumber: number,
+  thirdNumber: number
+) {
+  const sum = firstNumber + seconNumber + thirdNumber;
+  const output = sum % 1 === 0 ? `${sum} - Integer` : `${sum} - Float`;
+
+  console.log(output);
+}
+
+integerAndFloat(9, 100, 1.1);
+integerAndFloat(100, 200, 303);
