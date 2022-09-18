e0d612eb7c74550a7078d13a6e93e88c9986cbd0
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-18 14:37:43 +0300

    fuel calculation function

---
+++ src/02. Data Types and Variables/fuelMoney.ts
@@ -0,0 +1,10 @@
+function fuelMoney(distance: number, passengers: number, price: number) {
+  let needenFuel = (distance / 100) * 7;
+  needenFuel += passengers * 0.1;
+  const total = price * needenFuel;
+
+  console.log(`Needed money for that trip is ${total} lv`);
+}
+
+fuelMoney(260, 9, 2.49);
+fuelMoney(90, 14, 2.88);
