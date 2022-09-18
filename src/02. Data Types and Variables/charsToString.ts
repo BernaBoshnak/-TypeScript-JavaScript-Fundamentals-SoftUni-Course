7917dbceba2ee23530a78e8c0c5aef74c93ddb3f
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-18 14:04:30 +0300

    chars to string and convert meters to kilometres solution

---
+++ src/02. Data Types and Variables/charsToString.ts
@@ -0,0 +1,13 @@
+function charsToString(
+  firstLetter: string,
+  secondLetter: string,
+  lastLetter: string
+) {
+  const result = `${firstLetter}${secondLetter}${lastLetter}`;
+
+  console.log(result);
+}
+
+charsToString("a", "b", "c");
+charsToString("%", "2", "o");
+charsToString("1", "5", "p");
---
+++ src/02. Data Types and Variables/convertMToKm.ts
@@ -0,0 +1,6 @@
+function convertMToKm(num: number) {
+  return (num / 1000).toFixed(2);
+}
+
+console.log(convertMToKm(1852));
+console.log(convertMToKm(798));
