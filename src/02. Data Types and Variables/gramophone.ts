09de6b0f515c983780635f1a164e2458b443dc39
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-18 15:00:22 +0300

    calculate full rotation the plate

---
+++ src/02. Data Types and Variables/gramophone.ts
@@ -0,0 +1,9 @@
+function gramophone(bandName: string, albumName: string, songName: string) {
+  const time = (albumName.length * bandName.length * songName.length) / 2;
+  const rotations = Math.ceil(time / 2.5);
+
+  console.log(`The plate was rotated ${rotations} times.`);
+}
+
+gramophone("Black Sabbath", "Paranoid", "War Pigs");
+gramophone("Rammstein", "Sehnsucht", "Engel");
