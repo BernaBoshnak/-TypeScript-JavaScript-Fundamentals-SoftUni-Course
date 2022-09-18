3bc68a56ee079c166dd10c0c835c63e588dd0d80
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-18 23:52:57 +0300

    right place solution

---
+++ src/02. Data Types and Variables/rightPlace.ts
@@ -0,0 +1,9 @@
+function rightPlace(string: string, char: string, result: string) {
+  const word = string.replace("_", char);
+  const output = word === result ? "Matched" : "Not Matched";
+
+  console.log(output);
+}
+
+rightPlace("Str_ng", "I", "Strong");
+rightPlace("Str_ng", "i", "String");
