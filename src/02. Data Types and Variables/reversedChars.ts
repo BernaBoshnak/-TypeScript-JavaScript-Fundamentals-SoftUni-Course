dd535cd9b365499c733e75402af9fb29cad6d740
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-18 23:40:07 +0300

    sum and print type of the number (integer and float)

---
+++ src/02. Data Types and Variables/reversedChars.ts
@@ -0,0 +1,11 @@
+function reversedChars(
+  firstLetter: string,
+  secondLetter: string,
+  lastLetter: string
+) {
+  const result = `${lastLetter} ${secondLetter} ${firstLetter}`;
+  console.log(result);
+}
+
+reversedChars("A", "B", "C");
+reversedChars("1", "L", "&");
