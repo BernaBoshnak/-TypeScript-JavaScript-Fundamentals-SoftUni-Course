7179c004ba1672c86c6122389e06797bab705452
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-20 11:14:34 +0300

    non-decreasing subsequence solution

---
+++ src/03. Arrays/non-DecreasingSubsequence.ts
@@ -0,0 +1,14 @@
+function nonDecreasingSubsequence(array: number[]) {
+  let currentBiggestNum = array[0];
+  const result = array.filter((element) => {
+    if (element >= currentBiggestNum) {
+      currentBiggestNum = element;
+    }
+    return element >= currentBiggestNum;
+  });
+  console.log(result.join(" "));
+}
+
+nonDecreasingSubsequence([1, 3, 8, 4, 10, 12, 3, 2, 24]);
+nonDecreasingSubsequence([1, 2, 3, 4]);
+nonDecreasingSubsequence([20, 3, 2, 15, 6, 1]);
