afbc650fe58581847f1cfacfa7e134d1706c8bd8
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-21 00:02:50 +0300

    print n-th element solution

---
+++ src/03. Arrays/printN-thElement.ts
@@ -0,0 +1,15 @@
+function printElement(array: string[]) {
+  const newArray = [];
+  const lastElement = Number(array.pop());
+
+  for (let i = 0; i < array.length; i++) {
+    if (!(i % lastElement)) {
+      newArray.push(array[i]);
+    }
+  }
+  console.log(newArray.join(" "));
+}
+
+printElement(["5", "20", "31", "4", "20", "2"]);
+printElement(["dsa", "asd", "test", "test", "2"]);
+printElement(["1", "2", "3", "4", "5", "6"]);
