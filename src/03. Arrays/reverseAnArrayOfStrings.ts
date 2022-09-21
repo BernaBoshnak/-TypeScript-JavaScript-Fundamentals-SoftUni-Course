c0270d5d454a492edd49e8f5af33fc04599328d2
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-21 12:14:32 +0300

    reverse an array of strings solution

---
+++ src/03. Arrays/reverseAnArrayOfStrings.ts
@@ -0,0 +1,14 @@
+function reverseAnArrayOfStrings(input: string[]) {
+  let letters = input;
+  const newLetters = [];
+
+  for (let i = letters.length - 1; i >= 0; i--) {
+    newLetters.push(letters[i]);
+  }
+  letters = newLetters;
+  console.log(letters.join(" "));
+}
+
+reverseAnArrayOfStrings(["a", "b", "c", "d", "e"]);
+reverseAnArrayOfStrings(["abc", "def", "hig", "klm", "nop"]);
+reverseAnArrayOfStrings(["33", "123", "0", "dd"]);
