704bde4e7b6afcd135563baa826dc8839c39e087
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-22 23:09:29 +0300

    factorial division solution

---
+++ src/04. Functions/factorialDivision.ts
@@ -0,0 +1,17 @@
+function factorialDivision(firstNum: number, secondNum: number) {
+  const factorial = (num: number) => {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  };
+
+  const factorialOne = factorial(firstNum);
+  const factorialTwo = factorial(secondNum);
+  const result = factorialOne / factorialTwo;
+  console.log(result.toFixed(2));
+}
+
+factorialDivision(5, 2);
+factorialDivision(6, 2);
