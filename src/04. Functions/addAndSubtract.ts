a642e9377f45cf9afe0f5dec425e29983bc61688
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-22 20:49:53 +0300

    add and subtract solution

---
+++ src/04. Functions/addAndSubtract.ts
@@ -0,0 +1,12 @@
+const sum = (firtsNum: number, secondNum: number) => firtsNum + secondNum;
+const subtraction = (result: number, thirdNum: number) => result - thirdNum;
+
+function addAndSubtract(firtsNum: number, secondNum: number, thirdNum: number) {
+  const result = sum(firtsNum, secondNum);
+
+  return subtraction(result, thirdNum);
+}
+
+console.log(addAndSubtract(23, 6, 10));
+console.log(addAndSubtract(1, 17, 30));
+console.log(addAndSubtract(42, 58, 100));
