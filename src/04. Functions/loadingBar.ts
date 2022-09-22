101b6616371a41addb6c5afb60f79b7393ad5471
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-22 23:35:49 +0300

    loading bar solution

---
+++ src/04. Functions/loadingBar.ts
@@ -0,0 +1,14 @@
+function loadingBar(number: number) {
+  const temp = number / 10;
+
+  if (number === 100) {
+    console.log(`${number}% Complete!`);
+    console.log(`[${"%".repeat(temp)}]`);
+  } else {
+    console.log(`${number}% [${"%".repeat(temp)}${".".repeat(10 - temp)}]`);
+    console.log("Still loading...");
+  }
+}
+loadingBar(30);
+loadingBar(50);
+loadingBar(100);
