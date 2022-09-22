cf9753807a01e270d2a3056267636ad24f07fecb
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-22 22:56:05 +0300

    added step by step descriptive logic to print ascii value of all characters

---
+++ src/04. Functions/charactersInRange.ts
@@ -0,0 +1,20 @@
+function charactersInRange(start: string, end: string) {
+    let startValue = start.charCodeAt(0);
+    let endValue = end.charCodeAt(0);
+
+    if (startValue > endValue) {
+        const temp = startValue;
+        startValue = endValue;
+        endValue = temp;
+    }
+
+    let result = '';
+    for (let i = startValue + 1; i < endValue; i++) {
+        result += String.fromCharCode(i) + ' ';
+    }
+    return result;
+}
+
+console.log(charactersInRange('a', 'd'));
+console.log(charactersInRange('#', ':'));
+console.log(charactersInRange('C', '#'));
