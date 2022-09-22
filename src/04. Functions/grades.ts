348a75d197c764d0544963eb403953476ca0b705
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-22 23:22:52 +0300

    print grades

---
+++ src/04. Functions/grades.ts
@@ -0,0 +1,17 @@
+function grades(grade: number) {
+  if (grade <= 2.99) {
+    return "Fail (2)";
+  } else if (grade <= 3.49) {
+    return `Poor (${grade.toFixed(2)})`;
+  } else if (grade <= 4.49) {
+    return `Good (${grade.toFixed(2)})`;
+  } else if (grade <= 5.49) {
+    return `Very good (${grade.toFixed(2)})`;
+  } else {
+    return `Excellent (${grade.toFixed(2)})`;
+  }
+}
+
+console.log(grades(3.33));
+console.log(grades(4.5));
+console.log(grades(2.99));
