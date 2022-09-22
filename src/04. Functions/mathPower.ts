cc963c923917140dee872e3cc3ef7682ed96592f
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-22 23:47:48 +0300

    math power solution

---
+++ src/04. Functions/mathPower.ts
@@ -0,0 +1,11 @@
+function mathPower(num: number, power: number) {
+  let result = 1;
+
+  for (let i = 0; i < power; i++) {
+    result *= num;
+  }
+  return result;
+}
+
+console.log(mathPower(2, 8));
+console.log(mathPower(3, 4));
