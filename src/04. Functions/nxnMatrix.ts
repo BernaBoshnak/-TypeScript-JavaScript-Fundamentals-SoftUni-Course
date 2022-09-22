fd84473828c71fe1cf65133fa7d42c40bde8822a
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-22 23:58:29 +0300

    added function that receives a single integer number n and prints nxn matrix with that number

---
+++ src/04. Functions/nxnMatrix.ts
@@ -0,0 +1,16 @@
+function nxnMatrix(number: number) {
+  const firstLine = (number + " ").repeat(number);
+
+  const printMatrix = (number: number) => {
+    for (let i = 0; i < number; i++) {
+      console.log(firstLine);
+    }
+  };
+
+  const result = printMatrix(number);
+  return result;
+}
+
+nxnMatrix(3);
+nxnMatrix(7);
+nxnMatrix(2);
