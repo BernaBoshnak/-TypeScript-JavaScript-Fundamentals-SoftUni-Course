2fa142bcf9aa0c75786f4a76c24298c081241d51
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-23 11:42:24 +0300

    odd and even sum solution

---
+++ src/04. Functions/oddAndEvenSum.ts
@@ -0,0 +1,19 @@
+function oddAndEvenSum(number: number) {
+  const convert = number.toString();
+  const evenSum = (a: number) => !(a % 2);
+  let totalEvenSum = 0;
+  let totalOddSum = 0;
+
+  for (let i = 0; i < convert.length; i++) {
+    const currentNum = Number(convert[i]);
+    if (evenSum(currentNum)) {
+      totalEvenSum += currentNum;
+    } else {
+      totalOddSum += currentNum;
+    }
+  }
+  return `Odd sum = ${totalOddSum}, Even sum = ${totalEvenSum}`;
+}
+
+console.log(oddAndEvenSum(1000435));
+console.log(oddAndEvenSum(3495892137259234));
