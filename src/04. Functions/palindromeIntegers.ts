1c7bcd7d510d98f551e1ac74d36410115939f825
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-23 18:02:57 +0300

    function which checks if each integer is a palindrome or not

---
+++ src/04. Functions/palindromeIntegers.ts
@@ -0,0 +1,21 @@
+function palindromeIntegers(array: number[]) {
+  const isPalindrom = (text: string) => {
+    const length = text.length;
+    let isPalindromValue = true;
+
+    for (let i = 0; i < Math.floor(length / 2); i++) {
+      if (text[i] !== text[length - 1 - i]) {
+        isPalindromValue = false;
+        break;
+      }
+    }
+    return isPalindromValue;
+  };
+
+  for (const number of array) {
+    console.log(isPalindrom(number.toString()));
+  }
+}
+
+palindromeIntegers([123, 323, 421, 121]);
+palindromeIntegers([32, 2, 232, 1010]);
