97203ea306353074f3314a0ca5637020ac918e16
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-23 21:04:21 +0300

    repeat string solution

---
+++ src/04. Functions/repeatString.ts
@@ -0,0 +1,11 @@
+function repeatString(string: string, count: number) {
+  let result = "";
+
+  for (let i = 0; i < count; i++) {
+    result += string;
+  }
+  return result;
+}
+
+console.log(repeatString("abc", 3));
+console.log(repeatString("String", 2));
