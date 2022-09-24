0aef30e5a6f2d94045036471c60b252c3a3afe35
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-24 20:50:48 +0300

    process odd numbers solution

---
+++ src/05. Arrays Advanced/processOddNumbers.ts
@@ -0,0 +1,18 @@
+function processOddNumbers(numbers: number[]) {
+  const oddNumbers: number[] = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (i % 2 !== 0) {
+      oddNumbers.push(numbers[i]);
+    }
+  }
+  const result = oddNumbers
+    .map((b) => b * 2)
+    .reverse()
+    .join(" ");
+  console.log(result);
+}
+
+processOddNumbers([10, 15, 20, 25]);
+processOddNumbers([3, 0, 10, 4, 7, 3]);
+
+export {};
