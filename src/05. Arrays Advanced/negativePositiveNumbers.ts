ccf4613cd5bd4ca23fef8a4f48f974cd0e8a7645
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-24 18:12:29 +0300

    function that checks whether a number is positive or negative

---
+++ src/05. Arrays Advanced/negativePositiveNumbers.ts
@@ -0,0 +1,19 @@
+function negativeOrPositiveNumbers(numbers: number[]) {
+  const newArray = [];
+  for (const num of numbers) {
+    if (num >= 0) {
+      newArray.push(num);
+    } else {
+      newArray.unshift(num);
+    }
+  }
+
+  for (const element of newArray) {
+    console.log(element);
+  }
+}
+
+negativeOrPositiveNumbers([7, -2, 8, 9]);
+negativeOrPositiveNumbers([3, -2, 0, -1]);
+
+export {};
