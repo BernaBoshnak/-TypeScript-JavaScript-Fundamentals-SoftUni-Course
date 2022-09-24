7697d812c39afc3db27fbb76e7e21998bfc3d40b
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-24 15:18:08 +0300

    smallest number solution

---
+++ src/04. Functions/smallestNumbers.ts
@@ -0,0 +1,14 @@
+const smallestNum = (firtsNum: number, secondNum: number, thirdNum: number) =>
+  Math.min(firtsNum, secondNum, thirdNum);
+
+function smallestOfThreeNumbers(
+  firtsNum: number,
+  secondNum: number,
+  thirdNum: number
+) {
+  return smallestNum(firtsNum, secondNum, thirdNum);
+}
+
+console.log(smallestOfThreeNumbers(2, 5, 3));
+console.log(smallestOfThreeNumbers(600, 342, 123));
+console.log(smallestOfThreeNumbers(25, 21, 4));
