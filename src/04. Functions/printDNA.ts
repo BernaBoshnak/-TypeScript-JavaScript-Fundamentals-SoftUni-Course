db4f52d3bf341c632498863ab414cf94973e95a8
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-24 15:27:28 +0300

    print deoxyribonucleic acid solution

---
+++ src/04. Functions/printDNA.ts
@@ -0,0 +1,21 @@
+function printDNA(number: number) {
+  const sequence = "ATCGTTAGGG".split("");
+
+  for (let i = 1; i <= number; i++) {
+    const [a, b] = sequence.splice(0, 2);
+    if (i === 1 || i % 4 === 1) {
+      console.log(`**${a}${b}**`);
+    } else if (i === 2 || i % 4 === 2) {
+      console.log(`*${a}--${b}*`);
+    } else if (i === 3 || i % 4 === 3) {
+      console.log(`${a}----${b}`);
+    } else {
+      console.log(`*${a}--${b}*`);
+    }
+    sequence.push(a);
+    sequence.push(b);
+  }
+}
+
+printDNA(4);
+printDNA(10);
