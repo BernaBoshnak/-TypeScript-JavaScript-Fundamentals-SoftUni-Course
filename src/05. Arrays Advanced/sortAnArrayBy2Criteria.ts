5d125e9c52cffd6e60f224105b7a714c73458575
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-25 20:22:08 +0300

    added function that sorts an array of strings

---
+++ src/05. Arrays Advanced/sortAnArrayBy2Criteria.ts
@@ -0,0 +1,17 @@
+function sortAnArrayBy2Criteria(array: string[]) {
+  const sorted = array.slice(0).sort((a, b) => {
+    if (a.length === b.length) {
+      return a.localeCompare(b);
+    } else {
+      return a.length - b.length;
+    }
+  });
+  return sorted.join("\n");
+}
+
+console.log(sortAnArrayBy2Criteria(["alpha", "beta", "gamma"]));
+console.log(
+  sortAnArrayBy2Criteria(["Isacc", "Theodor", "Jack", "Harrison", "George"])
+);
+
+export {};
