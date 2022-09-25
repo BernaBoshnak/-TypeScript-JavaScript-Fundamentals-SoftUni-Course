b71e9546f9d3f0371f85f97e3c6674cf8cbd5ec2
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-25 21:22:20 +0300

    search for a number solution

---
+++ src/05. Arrays Advanced/searchNumber.ts
@@ -0,0 +1,20 @@
+type Target = [number, number, number];
+
+function searchNumber(firstArray: number[], secondArray: Target) {
+  const output = firstArray.slice(0, secondArray[0]);
+  output.splice(0, secondArray[1]);
+  const number = secondArray[2];
+  let count = 0;
+
+  for (let i = 0; i < output.length; i++) {
+    if (output[i] === secondArray[2]) {
+      count++;
+    }
+  }
+
+  return `Number ${number} occurs ${count} times.`;
+}
+
+console.log(searchNumber([5, 2, 3, 4, 1, 3, 6], [6, 2, 3]));
+
+export {};
