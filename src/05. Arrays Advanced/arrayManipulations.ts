6462537f928fdcf3acd71fa44baec88851978c71
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-25 11:03:43 +0300

    manipulated array solution

---
+++ src/05. Arrays Advanced/arrayManipulations.ts
@@ -0,0 +1,50 @@
+function arrayManipulations(commands: Array<string>) {
+  let array = commands.shift()?.split(" ").map(Number);
+
+  for (let i = 0; i < commands.length; i++) {
+    const command = commands[i].split(" ");
+
+    const firstNum = Number(command[1]);
+    const secondNum = Number(command[2]);
+    switch (command[0]) {
+      case "Add":
+        add(firstNum);
+        break;
+      case "Remove":
+        remove(firstNum);
+        break;
+      case "RemoveAt":
+        removeAt(firstNum);
+        break;
+      case "Insert":
+        insert(firstNum, secondNum);
+        break;
+    }
+  }
+  function add(element: number) {
+    array?.push(element);
+  }
+
+  function remove(num: number) {
+    array = array?.filter((element) => element !== num);
+  }
+
+  function removeAt(index: number) {
+    array?.splice(index, 1);
+  }
+
+  function insert(num: number, index: number) {
+    array?.splice(index, 0, num);
+  }
+  console.log(array?.join(" "));
+}
+
+arrayManipulations([
+  "4 19 2 53 6 43",
+  "Add 3",
+  "Remove 2",
+  "RemoveAt 1",
+  "Insert 8 3",
+]);
+
+export {};
