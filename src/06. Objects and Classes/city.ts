61f3fa99626a03bc9de50f0f210170638e22def2
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-26 11:41:56 +0300

    added a city object and printed the technical keys and values

---
+++ src/06. Objects and Classes/city.ts
@@ -0,0 +1,32 @@
+interface City {
+  [key: string]: string | number;
+  name: string;
+  area: string | number;
+  population: string | number;
+  country: string;
+  postCode: string | number;
+}
+
+function city(
+  name: City["name"],
+  area: City["area"],
+  population: City["population"],
+  country: City["country"],
+  postCode: City["postCode"]
+) {
+  const cityObj: City = {
+    name: name,
+    area: Number(area),
+    population: Number(population),
+    country: country,
+    postCode: Number(postCode),
+  };
+
+  for (const key in cityObj) {
+    console.log(`${key} -> ${cityObj[key]}`);
+  }
+}
+
+city("Sofia", "492", "1238438", "Bulgaria", "1000");
+
+export {};
