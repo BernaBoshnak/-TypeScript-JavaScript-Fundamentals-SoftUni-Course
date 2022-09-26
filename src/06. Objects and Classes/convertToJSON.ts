fb40d0d7b4b631bb6c5cbd6519d9e61251adbc34
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-26 12:31:58 +0300

    convert to json solution

---
+++ src/06. Objects and Classes/convertToJSON.ts
@@ -0,0 +1,23 @@
+interface Info {
+  name: string;
+  lastName: string;
+  hairColor: string;
+}
+
+function convertToJSON(
+  name: Info["name"],
+  lastName: Info["lastName"],
+  hairColor: Info["hairColor"]
+) {
+  const person: Info = {
+    name: name,
+    lastName: lastName,
+    hairColor: hairColor,
+  };
+
+  console.log(JSON.stringify(person));
+}
+
+convertToJSON("George", "Jones", "Brown");
+
+export {};
