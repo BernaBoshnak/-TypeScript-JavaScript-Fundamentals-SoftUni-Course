d11002c0f87b9021479d4e9a54edbb0b523fea9d
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-09-26 18:25:02 +0300

    created employee class that receives in the constructor the name and the personal number which the length of the name

---
+++ src/06. Objects and Classes/employees.ts
@@ -0,0 +1,42 @@
+interface EmployeeData {
+  name: string;
+  personalNumber: number;
+}
+
+function employees(listEmployees: string[]) {
+  class Employee implements EmployeeData {
+    name: EmployeeData["name"];
+    personalNumber: EmployeeData["personalNumber"];
+    constructor(
+      name: EmployeeData["name"],
+      number: EmployeeData["personalNumber"]
+    ) {
+      this.name = name;
+      this.personalNumber = number;
+    }
+
+    allListEmployees() {
+      return `Name: ${this.name} -- Personal Number: ${this.personalNumber}`;
+    }
+  }
+  const personInfo: Array<Employee> = [];
+
+  for (let i = 0; i < listEmployees.length; i++) {
+    const employeeName = listEmployees[i];
+    const personalNumber = listEmployees[i].length;
+    personInfo.push(new Employee(employeeName, personalNumber));
+  }
+
+  for (const person of personInfo) {
+    console.log(person.allListEmployees());
+  }
+}
+
+employees([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
+
+export {};
