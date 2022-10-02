8b1f5447c17c523037bf3fb6a3d008cea318bda8
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-10-02 13:43:05 +0300

    added word occurrences solution

---
+++ src/07. Associative Arrays/wordOccurrences.ts
@@ -0,0 +1,40 @@
+function wordOccurrences(array: string[]) {
+  const words = new Map<string, number>();
+
+  array.forEach((word) => {
+    if (!words.has(word)) {
+      words.set(word, 0);
+    }
+
+    const currentWord = words.get(word);
+
+    if (currentWord !== undefined) {
+      words.set(word, currentWord + 1);
+    }
+  });
+
+  const sorted = Array.from(words).sort((a, b) => b[1] - a[1]);
+
+  for (const [word, countWords] of sorted) {
+    console.log(`${word} -> ${countWords} times`);
+  }
+}
+
+wordOccurrences([
+  "Here",
+  "is",
+  "the",
+  "first",
+  "sentence",
+  "Here",
+  "is",
+  "another",
+  "sentence",
+  "And",
+  "finally",
+  "the",
+  "third",
+  "sentence",
+]);
+
+export {};
