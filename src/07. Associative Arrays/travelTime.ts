4a36100a84bcec658e75305d514082641a5b97bf
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-11-01 12:59:43 +0200

    added travel time solution

---
+++ src/07. Associative Arrays/travelTime.ts
@@ -0,0 +1,70 @@
+type Info = {
+  town: string;
+  cost: number;
+};
+type Country = string;
+type Travels = Record<Country, Info[]>;
+
+function travelTime(array: Array<string>) {
+  const generateDestination = (array: Array<string>) => {
+    const travels: Travels = {};
+
+    array.forEach((line) => {
+      const [country, town, price] = line.split(" > ");
+      const currentPrice = Number(price);
+
+      const info: Info = {
+        town,
+        cost: currentPrice,
+      };
+
+      if (!Object.prototype.hasOwnProperty.call(travels, country)) {
+        travels[country] = [info];
+      } else {
+        const countryInfos = travels[country];
+
+        if (countryInfos) {
+          const foundInfo = countryInfos.find((info) => info.town === town);
+          if (!foundInfo) {
+            countryInfos.push(info);
+          } else {
+            if (currentPrice < foundInfo.cost) {
+              foundInfo.cost = currentPrice;
+            }
+          }
+        }
+      }
+    });
+
+    return travels;
+  };
+
+  const destination = generateDestination(array);
+  const sortedCountries = [...Object.keys(destination)].sort((a, b) =>
+    a.localeCompare(b)
+  );
+
+  const townPrice = (obj: Info[]) => {
+    const result = [];
+
+    for (const info of Object.values(obj)) {
+      result.push(`${info.town} -> ${info.cost}`);
+    }
+
+    return result.join(" ");
+  };
+
+  for (const country of sortedCountries) {
+    console.log(`${country} -> ${townPrice(destination[country])}`);
+  }
+}
+
+travelTime([
+  "Bulgaria > Sofia > 500",
+  "Bulgaria > Sopot > 800",
+  "France > Paris > 2000",
+  "Albania > Tirana > 1000",
+  "Bulgaria > Sofia > 200",
+]);
+
+// export {};
