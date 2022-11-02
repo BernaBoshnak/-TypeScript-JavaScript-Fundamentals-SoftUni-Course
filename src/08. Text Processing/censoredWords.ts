6c29a47beb90fc5f66c704a6b6e8d7a822111654
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-11-02 20:32:21 +0200

    added censored words solution

---
+++ src/08. Text Processing/censoredWords.ts
@@ -0,0 +1,11 @@
+const censoredWords = (text: string, word: string) => {
+  while (text.includes(word)) {
+    text = text.replace(word, "*".repeat(word.length));
+  }
+
+  console.log(text);
+};
+
+censoredWords("A small sentence with some words", "small");
+
+export {};
